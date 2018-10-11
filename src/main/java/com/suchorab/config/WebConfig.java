a658e5f1ad1341c1b5c19b84f17f502cac82814f
Mateusz Suchorab <suchorab.mateusz@gmail.com>
2018-10-11 20:25:39 +0200

    working static resources

--- src/main/java/com/suchorab/config/WebConfig.java
+++ src/main/java/com/suchorab/config/WebConfig.java
@@ -1,11 +1,14 @@
 package com.suchorab.config;
 
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
@@ -13,7 +16,7 @@ import org.springframework.web.servlet.view.JstlView;
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.suchorab")
-public class WebConfig {
+public class WebConfig extends WebMvcConfigurerAdapter{
 
 
     @Bean
@@ -25,4 +28,10 @@ public class WebConfig {
         return viewResolver;
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
+    }
 }
--- src/main/webapp/WEB-INF/views/head.jsp
+++ src/main/webapp/WEB-INF/views/head.jsp
@@ -18,7 +18,7 @@
 
         <link href="<c:url value="/resources/css/bootstrap.css"/>" rel='stylesheet' type='text/css' />
         <!-- jQuery (necessary JavaScript plugins) -->
-        <script type='text/javascript' src="<c:url value="../resources/js/jquery-1.11.1.min.js"/>"></script>
+        <script type='text/javascript' src="<c:url value="/resources/js/jquery-1.11.1.min.js"/>"></script>
         <!-- Custom Theme files -->
         <link href="<c:url value="/resources/css/style.css"/>" rel='stylesheet' type='text/css' />
         <!-- Custom Theme files -->
